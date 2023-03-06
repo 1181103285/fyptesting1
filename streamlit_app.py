46cf0150ee455f384008c042d17b6ccb96d377f6
1181103285 <122731724+1181103285@users.noreply.github.com>
2023-03-06 12:11:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,21 +30,3 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=7)
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
-
-img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-st.text(f'({img.shape[1]}x{img.shape[0]})')
-st.header('colour detected')
-
-HSVImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-hsv = HSVImg[0, 0]
-
-HSVvalue = ''
-h = int(hsv[0])
-s = int(hsv[1]) / 255
-v = int(hsv[2]) / 255
-HSVvalue = str(h) + ',' + str(s) + ',' + str(v)
-
-colour_prediction = knn.predict([[h,s,v]])
-
-st.text('hsv value: ' + HSVvalue)
-st.text('colour name: ' + colour_prediction[0])
